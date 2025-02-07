e95457fd945792a381c707951d667daa0625d239
swadesh gupta <swadeshgupta18@gmail.com>
2025-02-07 20:10:09 +0530

    Loader interceptors and React to api crud operations

--- client-app/src/features/activities/dashboard/AcitivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/AcitivityDashboard.tsx
@@ -15,14 +15,16 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, deleteActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit}: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, deleteActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting}: Props) {
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList activities={activities} 
           selectActivity={selectActivity}
-          deleteActivity = {deleteActivity}/>
+          deleteActivity = {deleteActivity}
+          submitting={submitting}/>
       </Grid.Column>
       <Grid.Column width="6">
         {selectedActivity && !editMode &&
@@ -30,7 +32,11 @@ export default function ActivityDashboard({ activities, selectedActivity, select
         cancelSelectActivity={cancelSelectActivity}
         openForm={openForm}
         />}
-        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit = {createOrEdit}/>}
+        {editMode && <ActivityForm 
+        closeForm={closeForm} 
+        activity={selectedActivity} 
+        createOrEdit = {createOrEdit}
+        submitting = {submitting}/>}
       </Grid.Column>
     </Grid>
   );
